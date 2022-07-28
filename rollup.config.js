20a5761aed021dea4042ad497488cea0f2ca2a49
mskian <veerfun23@gmail.com>
2022-07-28 11:03:01 +0530

    clean up and formats

--- rollup.config.js
+++ rollup.config.js
@@ -2,54 +2,75 @@ import commonjs from '@rollup/plugin-commonjs';
 import resolve from '@rollup/plugin-node-resolve';
 import livereload from 'rollup-plugin-livereload';
 import { terser } from 'rollup-plugin-terser';
+import json from '@rollup/plugin-json';
+import path from 'path';
+
+const onwarn = warning => {
+    if (
+        warning.code === 'CIRCULAR_DEPENDENCY' &&
+        !warning.importer.indexOf(path.normalize('node_modules/suffix-thumb/'))
+    ) {
+        return;
+    }
+
+    console.warn(`(!) ${warning.message}`);
+};
 
 const production = !process.env.ROLLUP_WATCH;
 
 function serve() {
-	let server;
+    let server;
 
-	function toExit() {
-		if (server) server.kill(0);
-	}
+    function toExit() {
+        if (server) server.kill(0);
+    }
 
-	return {
-		writeBundle() {
-			if (server) return;
-			server = require('child_process').spawn('npm', ['run', 'start', '--', '--dev'], {
-				stdio: ['ignore', 'inherit', 'inherit'],
-				shell: true
-			});
+    return {
+        writeBundle() {
+            if (server) return;
+            server = require('child_process').spawn(
+                'npm',
+                ['run', 'start', '--', '--dev'],
+                {
+                    stdio: ['ignore', 'inherit', 'inherit'],
+                    shell: true,
+                }
+            );
 
-			process.on('SIGTERM', toExit);
-			process.on('exit', toExit);
-		}
-	};
+            process.on('SIGTERM', toExit);
+            process.on('exit', toExit);
+        },
+    };
 }
 
 export default {
-	input: 'index.js',
-	output: {
-		sourcemap: true,
-		format: 'iife',
-		name: 'pronounceandspeech',
-		file: 'public/build/bundle.js'
-	},
-	plugins: [
+    input: 'index.js',
+    onwarn,
+    output: {
+        sourcemap: true,
+        format: 'iife',
+        name: 'pronounceandspeech',
+        file: 'public/build/bundle.js',
+    },
+    plugins: [
         resolve({
-			browser: true,
-		}),
-		commonjs({ include: 'node_modules/**'}),
-		// In dev mode, call `npm run start` once
-		// the bundle has been generated
-		!production && serve(),
-		// Watch the `public` directory and refresh the
-		// browser on changes when not in production
-		!production && livereload('public'),
-		// If we're building for production (npm run build
-		// instead of npm run dev), minify
-		production && terser()
-	],
-	watch: {
-		clearScreen: false
-	}
+            browser: true,
+        }),
+        commonjs({ include: 'node_modules/**' }),
+        json({
+            compact: true,
+        }),
+        // In dev mode, call `npm run start` once
+        // the bundle has been generated
+        !production && serve(),
+        // Watch the `public` directory and refresh the
+        // browser on changes when not in production
+        !production && livereload('public'),
+        // If we're building for production (npm run build
+        // instead of npm run dev), minify
+        production && terser(),
+    ],
+    watch: {
+        clearScreen: false,
+    },
 };
